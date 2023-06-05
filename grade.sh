e2a21069a2c36d28715c1c75f4f00df8832903fe
rcabotaje <130100627+rcabotaje@users.noreply.github.com>
2023-06-05 09:58:08 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -19,8 +19,19 @@ cp ListExamples.java ../grading-area
 cp ../TestListExamples.java ../grading-area
 cp -r ../lib ../grading-area
 
-javac ../grading-area/ListExamples.java 2> ../error.output.txt
+cd ..
+if [ -f TestListExamples.java ] 
+then 
+    echo 'file found';
+else 
+    echo '1 file not found'
+fi
+
+cd grading-area
+
+#javac ../grading-area/ListExamples.java 2> ../error.output.txt
 cd ../grading-area
+
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> ../error2.output.txt
 java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
